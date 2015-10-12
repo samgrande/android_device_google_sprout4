52d4c3cc97c36399c7c20dd00a1e3e99e9b37f68
Ujwal P <ujwalp15@users.noreply.github.com>
2015-10-12 21:49:31 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,14 @@
 # inherit from common sprout repo
 -include device/google/sprout-common/BoardConfigCommon.mk
 
+# kernel stuff
+TARGET_KERNEL_SOURCE := kernel/mediatek/sprout
+TARGET_KERNEL_CONFIG := cyanogenmod_sprout_defconfig
+BOARD_KERNEL_CMDLINE :=
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x00000100
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+
 TARGET_OTA_ASSERT_DEVICE := sprout,sprout4
 
 TARGET_RECOVERY_FSTAB := device/google/sprout4/rootdir/root/fstab.sprout
